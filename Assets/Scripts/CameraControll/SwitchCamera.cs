156f367c8c800f830793704ef6658ab8bb386e46
윤석 이 <nuclearbomb280@gmail.com>
2025-08-19 14:35:55 +0900

    feature : 카메라 전환 시 부드럽게 움직이게 하는 스크립트 추가

---
+++ Assets/Scripts/CameraControll/SwitchCamera.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using UnityEngine;
+
+public class SwitchCamera : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] _targets = null;
+
+    private GameObject _nowFllowingTarget;
+
+    private float _dt = 0;
+
+    [SerializeField]
+    private float _moveTime = 5;
+
+    private bool _isFollowing = false;
+
+    void Awake()
+    {
+        if (_targets == null)
+        {
+            _targets = new GameObject[0];
+            return;
+        }
+        _nowFllowingTarget = _targets[0];
+    }
+
+    private IEnumerator MoveToNewCamera()
+    {
+        _isFollowing = true;
+        while(true)
+        {
+            _dt += Time.fixedDeltaTime;
+            float nowStep = _dt / _moveTime;
+            if (nowStep >= 1)
+            {
+                break;
+            }
+            transform.position = Vector3.Lerp(transform.position, _nowFllowingTarget.transform.position, nowStep);
+            transform.rotation = Quaternion.Lerp(transform.rotation, _nowFllowingTarget.transform.rotation, nowStep);
+            yield return null;
+        }
+        _isFollowing = false;
+    }
+
+    public void SwitchToFollwingCamera()
+    {
+        _dt = 0;
+        if (_isFollowing == false)
+        {
+            StartCoroutine(MoveToNewCamera());
+        }
+    }
+
+    public void ChangeFollwingTarget(uint index)
+    {
+        if (_targets != null || index < _targets.Length)
+        {
+            _nowFllowingTarget = _targets[index];
+            SwitchToFollwingCamera();
+        }
+    }
+}
